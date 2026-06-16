9e9cfdeee9a12d38ee30c25812ec3eb1673770f3
sameerbathusha <sameerbathusha93@gmail.com>
2026-06-16 11:39:29 +0400

    Update config.js

--- config.js
+++ config.js
@@ -10,6 +10,6 @@
 // =============================================================================
 
 window.APP_CONFIG = {
-  SUPABASE_URL: "https://ugfzlkttghtfajjnkqak.supabase.co/rest/v1/",
+  SUPABASE_URL: "https://ugfzlkttghtfajjnkqak.supabase.co",
   SUPABASE_ANON_KEY: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InVnZnpsa3R0Z2h0ZmFqam5rcWFrIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODE1NjUwMjMsImV4cCI6MjA5NzE0MTAyM30.dlmKSAMIC34kTIdS5VcnkkZGYlbDXUggod4GfMwIu2w",
 };
